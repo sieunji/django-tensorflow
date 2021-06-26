3ff600238716f7d6cd4b9d55090be5de495d96af
sieunji <tldmswl1@naver.com>
2021-06-26 14:51:10 +0900

    code update

--- pandas_tutorial/basic.py
+++ pandas_tutorial/basic.py
@@ -13,3 +13,16 @@ ic(df)
 #리스트로 생성하는 방법
 df2 = pd.DataFrame([[1,2,3],[4,5,6]], index=[1,2], columns=['a','b','c'])
 ic(df2)
+
+def id(): return''.join(random.choice(string.ascii_letters)for i in range(5))
+def score(): return np.random.randint(0,101)
+
+#데이터 프레임을 생성자로 생성한 방식
+grade = pd.DataFrame([[score() for i in range(1,5)] for i in range(1,11)], index=[id() for i in range(1,11)], columns=['국어','영어','수학','과학'])
+ic(grade)
+
+#딕셔너리+loc를 활용해서 생성하는 방법
+grade2 = pd.DataFrame({"국어":score(), "영어":score(), "수학":score(), "과학":score() }, index=[id()])
+for i in range(1,11):
+    grade2.loc[id()] = {"국어":score(), "영어":score(), "수학":score(), "과학":score() }
+ic(grade2)
